c27bdb13f899904ce0aa15fecd3ab5f60681270d
Bence Mihucza <mihben@gmail.com>
2026-07-11 18:51:20 +0200

    refactor: Create extension for map grcp endpoint

--- src/Ironyx.Kernel.Sample/Program.cs
+++ src/Ironyx.Kernel.Sample/Program.cs
@@ -1,7 +1,6 @@
 
 using Ironyx.Kernel;
 using Ironyx.Kernel.Execution.Dispatchers;
-using Ironyx.Kernel.Receivers;
 using Ironyx.Kernel.Sample.Handlers;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +10,8 @@ builder.UseKernel()
 
 var app = builder.Build();
 
-app.MapGrpcService<GrpcEndpoint>();
+app.MapKernel();
+
 app.MapPost("/command", async context => await context.RequestServices.GetRequiredService<ICommandDispatcher>().DispatchAsync((await context.Request.ReadFromJsonAsync<SampleCommand>())!, context.RequestAborted));
 
 app.Run();
---
+++ src/Ironyx.Kernel/WebApplicationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Ironyx.Kernel.Receivers;
+using Microsoft.AspNetCore.Builder;
+
+namespace Ironyx.Kernel
+{
+    public static class WebApplicationExtensions
+    {
+        public static void MapKernel(this WebApplication application)
+        {
+            application.MapGrpcService<GrpcEndpoint>();
+        }
+    }
+}
