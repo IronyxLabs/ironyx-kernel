f7e62b2d12dffa1d2eba4ddf87c98ee5402cc075
Bence Mihucza <mihben@gmail.com>
2026-07-23 17:44:42 +0200

    refactor: Remove error handling from command receiver

--- src/Ironyx.Kernel/Endpoint/GrpcEndpoint.cs
+++ src/Ironyx.Kernel/Endpoint/GrpcEndpoint.cs
@@ -34,37 +34,10 @@ namespace Ironyx.Kernel.Receivers
             await _extractor.ExtractAsync(context.RequestHeaders, context.CancellationToken);
             using var scope = _logger.BeginScope(_requestContext.CreateLogContext());
 
-            try
-            {
-                await _commandDispatcher.DispatchAsync(await _deserializer.DeserializeAsync(envelop, context.CancellationToken), context.CancellationToken);
+            await _commandDispatcher.DispatchAsync(await _deserializer.DeserializeAsync(envelop, context.CancellationToken), context.CancellationToken);
 
-                _logger.LogDebug("Command accepted");
-                return GrpcReply.Accepted.Reply;
-            }
-            catch (ArgumentNullException exception) when (exception.ParamName == nameof(envelop.Type))
-            {
-                _logger.LogError(exception, "Error during receive command");
-                context.Status = GrpcReply.TypeIsNotDefined.Status;
-                return GrpcReply.TypeIsNotDefined.Reply;
-            }
-            catch (ArgumentNullException exception) when (exception.ParamName == nameof(envelop.Version))
-            {
-                _logger.LogError(exception, "Error during receive command");
-                context.Status = GrpcReply.VersionIsNotDefined.Status;
-                return GrpcReply.TypeIsNotDefined.Reply;
-            }
-            catch (NotSupportedException exception)
-            {
-                _logger.LogError(exception, "Error during receive command");
-                context.Status = GrpcReply.UnknownRequestType.Status;
-                return GrpcReply.UnknownRequestType.Reply;
-            }
-            catch (JsonException exception)
-            {
-                _logger.LogError(exception, "Error during receive command");
-                context.Status = GrpcReply.InvalidBody.Status;
-                return GrpcReply.InvalidBody.Reply;
-            }
+            _logger.LogDebug("Command accepted");
+            return GrpcReply.Accepted.Reply;
         }
 
         public override async Task<Reply> GetAsync(Envelop envelop, ServerCallContext context)
@@ -99,14 +72,6 @@ namespace Ironyx.Kernel.Receivers
 
         public static GrpcReply Accepted => new(new Status(StatusCode.OK, "Ok"),
                                                                 new Reply() { Status = "ACCEPTED" });
-        public static GrpcReply TypeIsNotDefined => new(new Status(StatusCode.InvalidArgument, "Request type is not defined"),
-                                                       new Reply() { Status = "ERROR", Error = new Error { Code = "TECH_REQUEST_TYPE_IS_MISSING", Message = "Request Type is not defined" } });
-        public static GrpcReply VersionIsNotDefined => new(new Status(StatusCode.InvalidArgument, "Version is not defined"),
-                                                       new Reply() { Status = "ERROR", Error = new Error { Code = "TECH_VERSION_IS_MISSING", Message = "Version is not defined" } });
-        public static GrpcReply UnknownRequestType => new(new Status(StatusCode.InvalidArgument, "Unknow request type"),
-                                                       new Reply() { Status = "ERROR", Error = new Error { Code = "TECH_UNKNOWN_REQUEST_TYPE", Message = "Unknow request type" } });
-        public static GrpcReply InvalidBody => new(new Status(StatusCode.InvalidArgument, "Invalid request body"),
-                                                       new Reply() { Status = "ERROR", Error = new Error { Code = "TECH_INVALID_REQUEST_BODY", Message = "Invalid request body" } });
         public static GrpcReply Ok(string data) => new(new Status(StatusCode.OK, "Ok"),
                                                                 new Reply() { Status = "OK", Data = data });
 
